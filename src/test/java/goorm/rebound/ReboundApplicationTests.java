a43d0d9c703dc0415a83cede34952a8fdc9036af
오준영 <ojy654@naver.com>
2025-09-04 01:34:05 +0900

    초기 세팅

---
+++ src/test/java/goorm/rebound/ReboundApplicationTests.java
@@ -0,0 +1,13 @@
+package goorm.rebound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReboundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
