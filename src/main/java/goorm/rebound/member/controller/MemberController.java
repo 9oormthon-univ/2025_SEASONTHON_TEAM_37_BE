2b466a012bea5baebcceabde0b12b9d2b2b8bb5b
오준영 <ojy654@naver.com>
2025-09-04 15:07:40 +0900

    MemberController 추가

---
+++ src/main/java/goorm/rebound/member/controller/MemberController.java
@@ -0,0 +1,37 @@
+package goorm.rebound.member.controller;
+
+import goorm.rebound.member.domain.Member;
+import goorm.rebound.member.dtos.JoinRequest;
+import goorm.rebound.member.dtos.LoginRequest;
+import goorm.rebound.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping("/join")
+    public ResponseEntity<String> join(@RequestBody JoinRequest request) {
+        Member member = memberService.join(request);
+        return ResponseEntity.ok("회원가입이 완료되었습니다 회원 아이디: " + member.getId());
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+        String token = memberService.login(request);
+        return ResponseEntity.ok(token);
+    }
+
+    //인증 테스트용 API
+    //login으로 받은 토큰을 헤더에 Key: Authorization, Value: Bearer <토큰 값>
+//    @GetMapping("/test")
+//    public ResponseEntity<String> test() {
+//        return ResponseEntity.ok("인증 성공!");
+//    }
+
+}
