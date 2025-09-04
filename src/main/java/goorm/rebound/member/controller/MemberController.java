bc29f7cad8db648dcc0b3eb7761db6c44ccc82ee
오준영 <ojy654@naver.com>
2025-09-04 15:11:28 +0900

    회원가입/로그인 검증 추가

--- src/main/java/goorm/rebound/member/controller/MemberController.java
+++ src/main/java/goorm/rebound/member/controller/MemberController.java
@@ -4,6 +4,7 @@ import goorm.rebound.member.domain.Member;
 import goorm.rebound.member.dtos.JoinRequest;
 import goorm.rebound.member.dtos.LoginRequest;
 import goorm.rebound.member.service.MemberService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,13 +17,13 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public ResponseEntity<String> join(@RequestBody JoinRequest request) {
+    public ResponseEntity<String> join(@Valid @RequestBody JoinRequest request) {
         Member member = memberService.join(request);
         return ResponseEntity.ok("회원가입이 완료되었습니다 회원 아이디: " + member.getId());
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request) {
         String token = memberService.login(request);
         return ResponseEntity.ok(token);
     }
