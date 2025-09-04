a285b771860cf26aa295f318ab770419bb869d42
오준영 <ojy654@naver.com>
2025-09-04 16:34:28 +0900

    MemberControllerAdvice 예외 처리 추가(DTO 값 invalid 시 예외 처리)

--- src/main/java/goorm/rebound/member/controller/MemberControllerAdvice.java
+++ src/main/java/goorm/rebound/member/controller/MemberControllerAdvice.java
@@ -2,6 +2,7 @@ package goorm.rebound.member.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -17,6 +18,22 @@ public class MemberControllerAdvice {
         responseEntity.put("status", HttpStatus.BAD_REQUEST.value());
         responseEntity.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
         responseEntity.put("message", e.getMessage());
+
+        return new ResponseEntity<>(responseEntity, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidErr(MethodArgumentNotValidException e) {
+        Map<String, Object> responseEntity = new HashMap<>();
+        responseEntity.put("status", HttpStatus.BAD_REQUEST.value());
+        responseEntity.put("error", HttpStatus.BAD_REQUEST.getReasonPhrase());
+
+        Map<String, String> messages = new HashMap<>();
+        e.getBindingResult().getFieldErrors().forEach(error -> {
+            messages.put(error.getField(), error.getDefaultMessage());
+        });
+
+        responseEntity.put("message",messages);
 
         return new ResponseEntity<>(responseEntity, HttpStatus.BAD_REQUEST);
     }
