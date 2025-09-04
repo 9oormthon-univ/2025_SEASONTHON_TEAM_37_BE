42f09975e66370a10aef5f02fe5b1daf0cb03654
Juhyun Park <pjh33188@chungbuk.ac.kr>
2025-09-04 21:54:58 +0900

    chore: 불필요한 패키지 삭제

--- src/main/java/rebound/backend/domain/post/repository/PostContentRepository.java
+++
@@ -1,7 +0,0 @@
-package rebound.backend.domain.post.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import rebound.backend.domain.post.entity.PostContent;
-
-public interface PostContentRepository extends JpaRepository<PostContent, Long> {
-}
