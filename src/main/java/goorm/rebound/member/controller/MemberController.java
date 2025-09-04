3b316bb9bbec9ff6a75e9b4048821f1aee4175f4
오준영 <ojy654@naver.com>
2025-09-04 15:41:29 +0900

    MemberController API response DTO 반환하도록 변경

--- src/main/java/goorm/rebound/member/controller/MemberController.java
+++ src/main/java/goorm/rebound/member/controller/MemberController.java
@@ -1,8 +1,9 @@
 package goorm.rebound.member.controller;
 
-import goorm.rebound.member.domain.Member;
-import goorm.rebound.member.dtos.JoinRequest;
-import goorm.rebound.member.dtos.LoginRequest;
+import goorm.rebound.member.dtos.requests.JoinRequest;
+import goorm.rebound.member.dtos.responses.JoinResponse;
+import goorm.rebound.member.dtos.requests.LoginRequest;
+import goorm.rebound.member.dtos.responses.LoginResponse;
 import goorm.rebound.member.service.MemberService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -17,15 +18,15 @@ public class MemberController {
     private final MemberService memberService;
 
     @PostMapping("/join")
-    public ResponseEntity<String> join(@Valid @RequestBody JoinRequest request) {
-        Member member = memberService.join(request);
-        return ResponseEntity.ok("회원가입이 완료되었습니다 회원 아이디: " + member.getId());
+    public ResponseEntity<JoinResponse> join(@Valid @RequestBody JoinRequest request) {
+        JoinResponse response = memberService.join(request);
+        return ResponseEntity.ok(response);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<String> login(@Valid @RequestBody LoginRequest request) {
-        String token = memberService.login(request);
-        return ResponseEntity.ok(token);
+    public ResponseEntity<LoginResponse> login(@Valid @RequestBody LoginRequest request) {
+        LoginResponse response = memberService.login(request);
+        return ResponseEntity.ok(response);
     }
 
     //인증 테스트용 API
