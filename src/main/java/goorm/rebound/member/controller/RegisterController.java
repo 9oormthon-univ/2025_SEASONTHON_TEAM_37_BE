47f61cdeff46d152987d169bd17fd24210754060
오준영 <ojy654@naver.com>
2025-09-04 20:56:42 +0900

    MemberCOntroller -> RegisterController 이름 변경

--- src/main/java/goorm/rebound/member/controller/RegisterController.java
+++ src/main/java/goorm/rebound/member/controller/RegisterController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1/members")
 @RequiredArgsConstructor
-public class MemberController {
+public class RegisterController {
 
     private final MemberService memberService;
 
