3360337662e107931723a00896aad94b3fffdc5e
ojy0903 <ojy654@naver.com>
2025-09-07 07:30:41 +0900

    cors

--- src/main/java/rebound/backend/config/WebConfig.java
+++ src/main/java/rebound/backend/config/WebConfig.java
@@ -10,9 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // 모든 경로에 대해
-                .allowedOrigins("http://localhost:3000", "http://221.167.238.133:3000", "https://api.reboundlabs.site", "https://reboundlabs.site") // 프론트엔드
-                                                                                                                        // Origin
-                                                                                                                        // 허용
+                .allowedOriginPatterns("http://localhost:3000", "http://221.167.238.133:3000", "https://api.reboundlabs.site", "https://reboundlabs.site") // 프론트엔드 Origin 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
